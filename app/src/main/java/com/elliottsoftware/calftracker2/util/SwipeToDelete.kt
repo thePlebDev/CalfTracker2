c0688313a41dbe3e36b7729b2a7deec41ec1dcf1
Tristan Elliott <>
2022-08-26 15:43:30 -0300

    swipe to delete animation implemented

--- app/src/main/java/com/elliottsoftware/calftracker2/util/SwipeToDelete.kt
+++ app/src/main/java/com/elliottsoftware/calftracker2/util/SwipeToDelete.kt
@@ -1,13 +1,15 @@
 package com.elliottsoftware.calftracker2.util
 
+import android.graphics.Canvas
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.elliottsoftware.calftracker2.R
 import com.elliottsoftware.calftracker2.models.Calf
 import com.elliottsoftware.calftracker2.recyclerViews.CalfListAdapter
 import com.elliottsoftware.calftracker2.viewModels.CalfViewModel
 
 class SwipeToDelete(private val calfViewModel: CalfViewModel, private val calfListAdapter: CalfListAdapter):
-    ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+    ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT) {
     override fun onMove(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder,
@@ -28,5 +30,16 @@ class SwipeToDelete(private val calfViewModel: CalfViewModel, private val calfLi
 
 
         //snackBarCreation.createSnackbarCalfDeleted(Globalview)
+    }
+
+
+
+    override fun onChildDraw(canvas: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                             dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            val viewItem = viewHolder.itemView
+            SwipeBackgroundHelper.paintDrawCommandToStart(canvas, viewItem, R.drawable.ic_trash, dX)
+        }
+        super.onChildDraw(canvas, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
     }
 }
